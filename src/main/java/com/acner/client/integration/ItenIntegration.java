fbe4c454e34a1b00c9cff0c71fe996ab56c258dd
Marcos Pereira <marcos.ant.pereira.jr@gmail.com>
2023-09-06 19:52:44 -0300

    feat: clients

---
+++ src/main/java/com/acner/client/integration/ItenIntegration.java
@@ -0,0 +1,17 @@
+package com.acner.client.integration;
+
+public class ItenIntegration {
+    private String number;
+
+    public ItenIntegration(String number) {
+        this.number = number;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
